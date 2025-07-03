d87c954f79564c4d83494869f081642e60109417
diegobejar2004 <diegobejar2004@gmail.com>
2025-07-02 22:25:29 -0600

    topics: domain: fix methods in resource repository

--- src/topics/domain/repositories/ResourceRepository.ts
+++ src/topics/domain/repositories/ResourceRepository.ts
@@ -2,5 +2,5 @@ import { ResourceI } from "../entititesI/ResourceI";
 
 export interface ResourceRepository {
     create(resource: ResourceI): Promise<ResourceI>;
-    findByTopic(topic_id: number): Promise<Array<ResourceI>>;
+    findOne(id: number): Promise<ResourceI>;
 }
