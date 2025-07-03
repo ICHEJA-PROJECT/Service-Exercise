1c4048507d5589e5e1df829b195d9156ce2c63dc
diegobejar2004 <diegobejar2004@gmail.com>
2025-07-02 22:32:31 -0600

    topics: domain: fix methods in repositories

--- src/topics/domain/repositories/TopicRepository.ts
+++ src/topics/domain/repositories/TopicRepository.ts
@@ -1,7 +1,9 @@
+import { Topic } from "../entities/Topic";
 import { TopicI } from "../entititesI/TopicI";
 
 export interface TopicRepository {
-    create(topic: TopicI): Promise<TopicI>;
+    create(topic: Topic): Promise<TopicI>;
     findAll(): Promise<Array<TopicI>>;
     findOne(id: number): Promise<TopicI>;
+    findByUnit(id: number): Promise<TopicI>;
 }
--- src/topics/domain/repositories/TopicResourceRepository.ts
+++ src/topics/domain/repositories/TopicResourceRepository.ts
@@ -2,4 +2,5 @@ import { TopicResourceI } from "../entititesI/TopicResourceI";
 
 export interface TopicResourceRepository {
     create(topicResource: TopicResourceI): Promise<TopicResourceI>;
+    findByTopic(id: number): Promise<TopicResourceI>;
 }
