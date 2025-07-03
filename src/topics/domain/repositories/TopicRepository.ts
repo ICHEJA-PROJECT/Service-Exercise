d66107d6677fddd071813a715961e9438ea696e8
diegobejar2004 <diegobejar2004@gmail.com>
2025-07-03 00:16:02 -0600

    topics: domain: fix in topic repository

--- src/topics/domain/repositories/TopicRepository.ts
+++ src/topics/domain/repositories/TopicRepository.ts
@@ -3,7 +3,7 @@ import { TopicI } from "../entititesI/TopicI";
 
 export interface TopicRepository {
     create(topic: Topic): Promise<TopicI>;
-    findAll(): Promise<Array<TopicI>>;
+    findAll(): Promise<Array<Partial<TopicI>>>;
     findOne(id: number): Promise<TopicI>;
     findByUnit(id: number): Promise<Array<Partial<TopicI>>>;
 }
