a6ed42ee86598e3071bcb0e9ddecb046579b3415
diegobejar2004 <diegobejar2004@gmail.com>
2025-07-03 09:48:34 -0600

    templates: domain: add relation in entity

--- src/templates/domain/entitiesI/TemplateI.ts
+++ src/templates/domain/entitiesI/TemplateI.ts
@@ -1,8 +1,12 @@
+import { ExerciseI } from "src/exercises/domain/entitiesI/ExerciseI";
+import { TopicI } from "src/topics/domain/entititesI/TopicI";
+
 export interface TemplateI {
     id: number;
     title: string;
     subtitle: string;
     content: string;
     suggest_time: string;
-    topic_id: number;
+    topic_id: TopicI;
+    exercises: ExerciseI[];
 }
