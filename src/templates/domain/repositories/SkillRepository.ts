003ecf3e9670786a6f0a57c7826bca8a57a12138
diegobejar2004 <diegobejar2004@gmail.com>
2025-07-11 11:22:52 -0600

    templates: domain: repositories fixed

--- src/templates/domain/repositories/SkillRepository.ts
+++ src/templates/domain/repositories/SkillRepository.ts
@@ -1,7 +1,9 @@
 import { CreateSkillDto } from "src/templates/data/dtos/create-skill.dto";
 import { SkillI } from "../entitiesI/SkillI";
+import { GetSkillsByTemplatesDto } from "src/templates/data/dtos/get-skills-by-templates.dto";
 
 export interface SkillRepository {
     create(createSkillDto: CreateSkillDto): Promise<SkillI>;
     findAll(): Promise<SkillI[]>;
+    findByTemplates(templateIds: number[]): Promise<SkillI[]>;
 }
--- src/templates/domain/repositories/TemplateRepository.ts
+++ src/templates/domain/repositories/TemplateRepository.ts
@@ -1,8 +1,9 @@
 import { CreateTemplateDto } from "src/templates/data/dtos/create-template.dto";
 import { TemplateI } from "../entitiesI/TemplateI";
+import { GetTemplatesByTopicsDto } from "src/templates/data/dtos/get-templates-by-topics.dto";
 
 export interface TemplateRepository {
     create(createTemplateDto: CreateTemplateDto): Promise<TemplateI>;
-    findByTopics(topics: number[]): Promise<TemplateI[]>;
+    findByTopics(topicIds: number[]): Promise<TemplateI[]>;
     findByTopic(topicId: number): Promise<TemplateI[]>;
 }
