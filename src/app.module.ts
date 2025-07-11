9da0ae5081874a9639d48ed38184225381c5fabf
diegobejar2004 <diegobejar2004@gmail.com>
2025-07-11 11:27:44 -0600

    project: delete basic files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TopicModule } from './topics/topic.module';
+import { LayoutModule } from './layouts/layout.module';
+import { TemplateModule } from './templates/template.module';
+import { ExerciseModule } from './exercises/exercise.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    password: 'Nanami04',
-    username: 'postgres',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    database: 'aprendia',
-    synchronize: true,
-    logging: true
-  })],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'Nanami04',
+      username: 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      database: 'aprendia',
+      synchronize: true,
+      logging: true
+    }),
+    TopicModule,
+    LayoutModule,
+    TemplateModule,
+    ExerciseModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
